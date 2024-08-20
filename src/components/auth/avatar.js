42265a77958110194cad9fea8a6e32392acca1a8
Aaron <aaronworsnop@gmail.com>
2024-08-20 18:48:53 +1200

    refactor: Change all JSX to JS files

--- src/components/auth/avatar.js
+++ src/components/auth/avatar.js
@@ -2,7 +2,7 @@
 
 import PropTypes from "prop-types"
 import Blockies from "react-blockies"
-import styles from "./styles/avatar.css"
+import "./styles/avatar.css"
 
 Avatar.propTypes = {
   size: PropTypes.number.isRequired,
